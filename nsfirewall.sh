15843982560841ae71c155c8eea83aaafdefcc7f
NSOPORTE <47125165+NSOPORTEDESARROLLO@users.noreply.github.com>
2020-06-20 12:41:09 -0600

    Add some YML
    - NETDATA YML
    - NSFIREWALL YML
    - PHPMYADMIN YML

--- nsfirewall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --name nsfirewall --net=host --privileged --restart=always \
-	 -v /data/etc/shorewall:/etc/shorewall \
-	 -v /etc/localtime:/etc/localtime:ro \
-	 -d nsoporte/nsfirewall
-
-
-exit 0
